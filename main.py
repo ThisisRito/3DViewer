f54ada9f9cfd817fb0eb8cf66714252d4473f238
ThisisRito <rito.xquare.wolfgang@gmail.com>
2022-08-09 16:56:16 +0900

    code format clean up

--- main.py
+++ main.py
@@ -1,36 +1,30 @@
 import sys
-
-from PySide2.QtGui import (QVector3D)
-from PySide2.Qt3DCore import (Qt3DCore)
-from PySide2.Qt3DExtras import (Qt3DExtras)
-
-import copy
-
-from PySide2.QtCore import(Property, Signal)
-
-
-from PySide2.QtWidgets import QApplication, QMainWindow, QPushButton
-from PySide2.QtWidgets import QLineEdit, QVBoxLayout, QWidget
-
-from PySide2.QtWidgets import QApplication, QMainWindow, QWidget
-from PySide2.QtWidgets import QVBoxLayout
-from PySide2.QtWidgets import QHBoxLayout
-from PySide2.QtWidgets import QFormLayout,QListWidget, QPushButton
-
-
-from PySide2.QtWidgets import QComboBox
-from PySide2.QtWidgets import QLabel
-
-from PySide2.QtGui import QColor,QQuaternion
-
-from PySide2.QtCore import(Property, Signal, Slot, QObject)
-
-from model import *
-
 from collections import defaultdict
 
+from PySide2.QtGui import QColor
+from PySide2.QtGui import QQuaternion
+from PySide2.QtGui import QVector3D
+from PySide2.QtCore import QObject
+from PySide2.Qt3DCore import Qt3DCore
+from PySide2.Qt3DExtras import Qt3DExtras
 
-import random
+from PySide2.QtWidgets import QApplication
+from PySide2.QtWidgets import QMainWindow
+from PySide2.QtWidgets import QPushButton
+from PySide2.QtWidgets import QLineEdit
+from PySide2.QtWidgets import QWidget
+from PySide2.QtWidgets import QLabel
+from PySide2.QtWidgets import QVBoxLayout
+from PySide2.QtWidgets import QHBoxLayout
+from PySide2.QtWidgets import QFormLayout
+from PySide2.QtWidgets import QListWidget
+from PySide2.QtWidgets import QComboBox
+
+
+
+
+from model import ObjectModel
+
 
 
 
@@ -51,7 +45,7 @@ class Entity(Qt3DCore.QEntity):
         self.addComponent(self._material)
 
     def set_shape(self,id,shape_info):
-        print("received set shape", id, shape_info)
+        print("Entity.set_shape receives", id, shape_info)
         if shape_info["shape"] == "Sphere":
             radius = shape_info["radius"]
             self._mesh = Qt3DExtras.QSphereMesh()
@@ -77,16 +71,14 @@ class Entity(Qt3DCore.QEntity):
         self._transform.setTranslation(QVector3D(x,y,z))
 
     def set_orientation(self,id,quaternion):
-        x,y,z,theta = quaternion;
+        x,y,z,theta = quaternion
 
         self._transform.setRotation(QQuaternion.fromAxisAndAngle(QVector3D(x,y,z),theta))
-        pass
 
     def set_rgb(self,id,rgb):
-        print('received')
+        print('Entity.set_rgb receives',id,rgb)
         r,g,b = rgb
-        self._material.setDiffuse(QColor(r,g,b));
-        print(self._material.diffuse().getRgb())
+        self._material.setDiffuse(QColor(r,g,b))
 
     def get_rgb(self):
         r,g,b,a = self._material.diffuse().getRgb()
@@ -111,7 +103,6 @@ class ObjectView(Qt3DExtras.Qt3DWindow):
         self.camController.setCamera(self.camera())
 
 
-
     def insert(self,id,entity):
         self.entities[id] = entity
         return
@@ -125,34 +116,11 @@ class ObjectView(Qt3DExtras.Qt3DWindow):
         del self.entities[id]
 
 
-    #def set_position(self,id,pos):
-        #return
-        #x,y,z = pos
-        #self.entity._transform.setTranslation(QVector3D(x,y,z))
-
-    #def get_position(self,id):
-        #return
-        #pass
-
-    #def set_rgb(self,id,rgb):
-        #return
-        #r,g,b = rgb
-        #self.entity._material.setDiffuse(QColor(r,g,b));
-
-    #def get_rgb(self,id):
-        #return
-        #r,g,b,a = entity._material.diffuse().getRgb()
-        #return (r,g,b)
-
-
-
-
 class ObjectsController(QObject):
     def __init__(self):
         super(ObjectsController,self).__init__()
         self.view = ObjectView()
         self.model = None
-        #self.model.remove.connect(self.remove)
 
     def setParent(self, parent):
         self.view.setParent(parent)
@@ -160,7 +128,6 @@ class ObjectsController(QObject):
         self.model = model
         self.model.Insert.connect(self.insert)
         self.model.Remove.connect(self.remove)
-        pass
 
 
     def insert(self,id,object):
@@ -171,22 +138,18 @@ class ObjectsController(QObject):
         entity.set_position(id,object.data["position"])
         self.view.insert(id,entity)
 
-        object.positionChanged.connect(entity.set_position);
-        object.colorChanged.connect(entity.set_rgb);
-        object.shapeChanged.connect(entity.set_shape);
-        object.orientationChanged.connect(entity.set_orientation);
-        pass
-
-    #def update(self,object):
-        #pass
+        object.positionChanged.connect(entity.set_position)
+        object.colorChanged.connect(entity.set_rgb)
+        object.shapeChanged.connect(entity.set_shape)
+        object.orientationChanged.connect(entity.set_orientation)
 
     def remove(self,id):
         entity = self.view.entities[id]
         object = self.model.objects[id]
-        object.positionChanged.disconnect(entity.set_position);
-        object.colorChanged.disconnect(entity.set_rgb);
-        object.shapeChanged.disconnect(entity.set_shape);
-        object.orientationChanged.disconnect(entity.set_orientation);
+        object.positionChanged.disconnect(entity.set_position)
+        object.colorChanged.disconnect(entity.set_rgb)
+        object.shapeChanged.disconnect(entity.set_shape)
+        object.orientationChanged.disconnect(entity.set_orientation)
         self.view.remove(id)
         return
 
@@ -202,22 +165,22 @@ class ListView(QWidget):
 
         self.buttonWidget = QWidget()
         self.buttonWidget.setLayout(self.buttonLayout)
-        self.listWidget = QListWidget();
+        self.listWidget = QListWidget()
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.buttonWidget)
         self.layout.addWidget(self.listWidget)
-        self.setLayout(self.layout);
+        self.setLayout(self.layout)
         self.setFixedWidth(100)
 
     def getSelctedRow(self):
-        return self.listWidget.currentRow();
+        return self.listWidget.currentRow()
 
     def takeItem(self, row):
         return self.listWidget.takeItem(row)
 
     def addItem(self, item):
-        return self.listWidget.addItem(item);
+        return self.listWidget.addItem(item)
 
 
 class ListViewController(QObject):
@@ -238,9 +201,7 @@ class ListViewController(QObject):
 
     def focus(self):
         row = self.view.getSelctedRow()
-        print(row,self.view.layout.count())
         id = self.ids[row]
-        print("calling: focus on", id);
         self.model.focus(id)
 
     def set_parent(self,par):
@@ -252,11 +213,11 @@ class ListViewController(QObject):
         self.model.Remove.connect(self.remove)
 
     def insert(self, id, object):
+        print("ListView.insert receives",id,object)
         self.ids.append(id)
         name = object.data["name"]
         self.view.addItem(name)
         object.nameChanged.connect(self.change_name_display)
-        print("receive")
 
     def remove(self, id):
         row = self.ids.index(id)
@@ -276,11 +237,9 @@ class ListViewController(QObject):
         name = item.text()
         selected_row = self.view.getSelctedRow()
         id = self.ids[selected_row]
-        self.model.set_name(id,name);
-        #TODO
+        self.model.set_name(id,name)
 
     def add_button_is_clicked(self):
-        print("send")
         self.model.insert()
 
     def del_button_is_clicked(self):
@@ -297,7 +256,6 @@ class DataView(QWidget):
         self.editors = {}
 
     def clear(self):
-        #self.layout = QFormLayout();
         self.editors  = {}
         while self.layout.count() > 0:
             self.layout.removeRow(0)
@@ -345,17 +303,10 @@ class PanelView(QWidget):
             self.size_view.set_dict(None,keys,values)
 
     def clear(self):
-        #self.layout = QFormLayout()
-        to_delete = [self.layout.itemAt(i) for i in range(self.layout.count())]
-        for item in to_delete:
+        while self.layout.count() > 0:
+            item = self.layout.itemAt(0)
             self.layout.removeItem(item)
-
-        #while self.layout.count() > 0:
-            #item = self.layout.itemAt(0)
-            #self.layout.removeItem(item)
-            #self.layout.removeRow(0)
-
-    #PySide2.QtWidgets.QComboBox.currentIndexChanged(index)
+            self.layout.removeRow(0)
 
     def display(self, data):
         self.clear()
@@ -384,7 +335,6 @@ class PanelView(QWidget):
         values = data["quaternion"]
         self.orientation_view.set_dict("Quaternion",keys,values)
 
-
         self.layout.addRow(self.size_view.layout)
         self.layout.addRow(self.rgb_view.layout)
         self.layout.addRow(self.position_view.layout)
@@ -392,7 +342,6 @@ class PanelView(QWidget):
         self.layout.addRow(self.shape_box)
         self.layout.addRow(self.apply_button)
         self.setLayout(self.layout)
-
 
 
 class PanelController(QObject):
@@ -414,10 +363,10 @@ class PanelController(QObject):
 
         buffer = defaultdict(None)
 
-        for key, editor in self.view.size_view.editors.items(): buffer[key] = editor.text();
-        for key, editor in self.view.rgb_view.editors.items(): buffer[key] = editor.text();
-        for key, editor in self.view.position_view.editors.items(): buffer[key] = editor.text();
-        for key, editor in self.view.orientation_view.editors.items(): buffer[key] = editor.text();
+        for key, editor in self.view.size_view.editors.items(): buffer[key] = editor.text()
+        for key, editor in self.view.rgb_view.editors.items(): buffer[key] = editor.text()
+        for key, editor in self.view.position_view.editors.items(): buffer[key] = editor.text()
+        for key, editor in self.view.orientation_view.editors.items(): buffer[key] = editor.text()
 
         if(shape == "Sphere"):
             data["radius"] = buffer["radius"]
@@ -436,7 +385,6 @@ class PanelController(QObject):
         self.model.update(data)
 
     def update(self,id):
-        #self.data = self.model.objects[id].data
         self.view.display(self.model.objects[id].data)
 
     def set_model(self,model):
@@ -445,24 +393,18 @@ class PanelController(QObject):
         self.model.Update.connect(self.update)
 
     def change_focus(self,id):
-        print("receiving: focus on", id);
+        print("PanelController.change_focus receives",id)
         if(id == -1):
             self.view.apply_button.clicked.disconnect(self.update_object)
             self.view.clear()
         else:
-            #self.data = copy.deepcopy(self.model.objects[id].data)
             self.view.display(self.model.objects[id].data)
             self.view.apply_button.clicked.connect(self.update_object)
-        pass
-
-
-
 
 
 
 class MainController(QMainWindow):
     backup = "/tmp/3DView.json"
-
     def __init__(self):
         super(MainController, self).__init__()
         self.setWindowTitle("3DViewer")
@@ -470,25 +412,24 @@ class MainController(QMainWindow):
         self.model = ObjectModel()
         self.model.backup = self.backup
 
-
         self.objects_controller = ObjectsController()
         self.objects_controller.set_model(self.model)
 
         self.objects_contianer = QWidget.createWindowContainer(self.objects_controller.view)
-        self.objects_contianer.setParent(self);
+        self.objects_contianer.setParent(self)
         self.objects_contianer.setFixedWidth(500)
         self.objects_contianer.setFixedHeight(300)
 
         self.list_controller = ListViewController()
         self.list_controller.set_model(self.model)
 
-        self.panel_controller = PanelController();
+        self.panel_controller = PanelController()
         self.panel_controller.set_model(self.model)
 
         self.layout = QHBoxLayout()
         self.layout.addWidget(self.list_controller.view)
         self.layout.addWidget(self.objects_contianer)
-        self.layout.addWidget(self.panel_controller.view);
+        self.layout.addWidget(self.panel_controller.view)
 
         widget = QWidget()
         widget.setLayout(self.layout)
@@ -498,9 +439,8 @@ class MainController(QMainWindow):
 
 
 if __name__ == '__main__':
-    #app = QGuiApplication(sys.argv)
     app = QApplication(sys.argv)
-    main_window = MainController();
+    main_window = MainController()
     main_window.show()
     sys.exit(app.exec_())
 
